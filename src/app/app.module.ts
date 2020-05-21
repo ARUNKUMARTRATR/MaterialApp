f01f306c9f423d8f16d45a72b39cdd0ea7f7b02f
ARUNKUMARTRATR <arunkumar.tr@experionglobal.com>
2020-05-21 20:25:32 +0530

    new component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TablePageComponent } from './table-page/table-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablePageComponent
   ],
   imports: [
+    MatButtonModule,
+    MatTableModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
